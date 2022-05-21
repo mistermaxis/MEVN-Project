41dc750e0c3498254f63af7b7b98df1ab3886ced
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-21 00:20:33 -0300

    Set up database connection

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log('Connected to Database'));
 
 const app = express();
 
