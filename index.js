c36f539c76ec0b0c062de112282c86eeb4a35621
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-21 13:59:20 -0300

    Setup swagger

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import swaggerJSDoc from 'swagger-jsdoc';
+import { serve, setup } from 'swagger-ui-express';
+
+const PORT = 4000;
+
 import express from 'express';
 import mongoose from 'mongoose';
 import clientsRouter from './routes/clients.js';
 import providersRouter from './routes/providers.js';
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      version: '1.0.1',
+      title: 'Client API',
+      description: 'An API to handle clients and their providers.',
+      contact: {
+        name: 'Maximiliano Diaz',
+        email: 'maximiliano_diaz@outlook.com.ar'
+      },
+      license: {
+        name: 'MIT',
+        url: 'https://mit-license.org'
+      },
+      servers: [`localhost:${PORT}`]
+    }
+  },
+  apis: ['.routes/*.js']
+}
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
 
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
@@ -15,7 +42,8 @@ db.once('open', () => console.log('Connected to Database'));
 const app = express();
 
 app.use(express.json());
+app.use('/api-docs', serve, setup(swaggerDocs));
 app.use('/clients', clientsRouter);
 app.use('/providers', providersRouter);
 
-app.listen(3000, () => console.log('Server Started'));
+app.listen(PORT, () => console.log('Server Started'));
