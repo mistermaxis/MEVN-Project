8bbc6a3b87a5f25597cfbae965e18d23c8971388
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-22 16:15:14 -0300

    Create table basic layout

---
+++ client/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app-header">
+    <h1>Clients</h1>
+    <button type="button">New Client</button>
+  </div>
+</template>
+
+<style scoped>
+.app-header {
+  background-color: aliceblue;
+  border: 1px lightgray solid;
+  border-bottom: none;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 3%;
+}
+
+h1 {
+  color: darkslategrey;
+}
+
+button {
+  border-color: lightgray;
+  border-width: 1px;
+  border-radius: 0.5rem;
+  padding: 0.5rem 1rem;
+}
+</style>
---
+++ client/src/components/TableHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="table-header">
+    <header class="header-item">Name</header>
+    <header class="header-item">Email</header>
+    <header class="header-item">Phone</header>
+    <header class="header-item">Providers</header>
+    <header class="header-item"></header>
+  </div>
+</template>
+
+<style scoped>
+  .table-header {
+    display: grid;
+    grid-template-columns: 20% 20% 20% 30% 10%;
+  }
+
+  .header-item {
+    border: 1px lightgray solid;
+    padding: 0.5rem;
+    padding-bottom: 0.3rem;
+  }
+</style>
