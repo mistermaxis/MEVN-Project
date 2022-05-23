9735c1e8f1769476dce64dc778cceec94fca0677
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-23 15:00:11 -0300

    Add clients table component

---
+++ client/src/components/ClientTable.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p v-for="provider in providers">{{provider.name}}</p>
+    <p v-if="error">{{error}}</p>
+  </div>
+</template>
+
+<script setup>
+import useClients from '../modules/data-api';
+
+const { providers, error, load} = useClients();
+
+await load();
+</script>
