4063e5475eb65b810fbc6ad7dedc0f59ae4d9972
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-06-04 11:51:24 -0300

    Add methods to create/delete users and providers

--- client/src/modules/data-api.js
+++ client/src/modules/data-api.js
@@ -1,22 +1,94 @@
-import { reactive, toRefs } from 'vue';
+import { readonly, ref } from 'vue';
+
+const state = ref({
+  clients: [],
+  providers: [],
+  error: null
+});
 
 export default function useClients() {
-  const state = reactive({
-    clients: [],
-    providers: [],
-    error: null
-  });
 
-  const load = async () => {
+  async function load() {
     try {
       const res = await fetch('http://localhost:4000/clients');
       const jsonResponse = await res.json();
-      state.clients = jsonResponse.clients;
-      state.providers = jsonResponse.providers;
+      state.value.clients = jsonResponse.clients;
+      state.value.providers = jsonResponse.providers;
     } catch (e) {
-      state.error = e;
+      state.value.error = e;
     }
-  };
+  }
 
-  return { ...toRefs(state), load }
+  async function insertClient(newClient) {
+    try {
+      const res = await fetch('http://localhost:4000/clients', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: newClient,
+      });
+      await res.json();
+      await load();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async function insertProvider(newProvider) {
+    try {
+      const res = await fetch('http://localhost:4000/providers', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: newProvider,
+      });
+      await res.json();
+      await load();
+      console.log(state.value.providers);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async function updateProvider(newProvider, providerId) {
+    try {
+      const res = await fetch(`http://localhost:4000/providers/${providerId}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: newProvider,
+      });
+      await res.json();
+      await load();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async function destroyProvider(providerId) {
+    try {
+      const res = await fetch(`http://localhost:4000/providers/${providerId}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      });
+      await res.json();
+      await load();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return {
+    state: readonly(state),
+    load,
+    insertClient,
+    insertProvider,
+    destroyProvider,
+    updateProvider
+  }
 }
