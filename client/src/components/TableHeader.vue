1bb6eba9b3c3ff4347606e6926bf3c6ae9845b27
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-06-04 11:51:53 -0300

    Update style for table header

--- client/src/components/TableHeader.vue
+++ client/src/components/TableHeader.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="table-header">
-    <header class="header-item">Name</header>
-    <header class="header-item">Email</header>
-    <header class="header-item">Phone</header>
-    <header class="header-item">Providers</header>
+    <header title="Name" class="header-item">Name</header>
+    <header title="Email" class="header-item">Email</header>
+    <header title="Phone" class="header-item">Phone</header>
+    <header title="Providers" class="header-item">Providers</header>
     <header class="header-item"></header>
   </div>
 </template>
@@ -11,7 +11,7 @@
 <style scoped>
 .table-header {
   display: grid;
-  grid-template-columns: 20% 20% 20% 30% 10%;
+  grid-template-columns: minmax(10%, 18%) minmax(15%, 24%) minmax(15%, 18%) minmax(20%, 40%) minmax(2.5rem, 3rem);
   font-weight: bold;
 }
 
@@ -21,6 +21,7 @@
   border-width: 1px 0px 1px 1px;
   padding: 0.5rem;
   padding-bottom: 0.3rem;
+  word-break: break-all;
 }
 
 .header-item:last-of-type {
