c05e0344ee32ffc24fc9292421354d9794bf0da6
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-06-04 23:31:23 -0300

    Move footer layout and style to Footer component

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    <a href="https://www.flaticon.com/free-icons/edit" title="edit icons">Edit icons created by Becris - Flaticon</a>
+    <a href="https://www.flaticon.com/free-icons/delete" title="delete icons">Delete icons created by Google - Flaticon</a>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1rem;
+}
+
+footer > a {
+  color: gray;
+  font-size: small;
+  text-align: center;
+}
+</style>
